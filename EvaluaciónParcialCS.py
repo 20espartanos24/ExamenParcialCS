578193bc377e649ea0f197bd9b213d318534e64a
20espartanos24 <20espartanos24@gmail.com>
2024-05-09 14:52:02 -0500

    Esta es nuestra primera version del proyecto

---
+++ EvaluaciónParcialCS.py
@@ -0,0 +1,11 @@
+class CalculadoraSueldo:
+    def __init__(self, nombre_trabajador, sueldo_basico, dias_falta, minutos_tardanza, horas_extras):
+              
+        self.nombre_trabajador = nombre_trabajador
+        self.sueldo_basico = sueldo_basico
+        self.dias_falta = dias_falta
+        self.minutos_tardanza = minutos_tardanza
+        self.horas_extras = horas_extras
+
+    
+
